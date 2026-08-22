9212595fce520aa52ab9f8c244edcca63a0851ba
NexusOS Developer <barberousseqc@gmail.com>
2026-08-22 14:58:42 -0400

    Enable NextOS Plymouth theme safely

--- build_files/build.sh
+++ build_files/build.sh
@@ -27,3 +27,7 @@ dnf5 install -y tmux
 systemctl enable podman.socket
 dnf5 config-manager setopt "terra-mesa".enabled=false
 sed -i "/terra-mesa/,/^$/ s/^enabled=.*/enabled=0/" /etc/yum.repos.d/*.repo
+
+if command -v plymouth-set-default-theme; then
+    plymouth-set-default-theme nextos
+fi
